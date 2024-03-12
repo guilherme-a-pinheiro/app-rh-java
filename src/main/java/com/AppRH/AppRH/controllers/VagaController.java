3c82d30eb3269afaff8a63cfa3e5657c1f67a7d5
Guilherme Alves Pinheiro <guiapinheiro29@gmail.com>
2024-03-11 23:34:37 -0300

    finalizando vagaController e candidato repository

--- src/main/java/com/AppRH/AppRH/controllers/VagaController.java
+++ src/main/java/com/AppRH/AppRH/controllers/VagaController.java
@@ -1,14 +1,19 @@
 package com.AppRH.AppRH.controllers;
 
+import com.AppRH.AppRH.models.Candidato;
 import com.AppRH.AppRH.models.Vaga;
 import com.AppRH.AppRH.repository.CandidatoRepository;
 import com.AppRH.AppRH.repository.VagaRepository;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @Controller
 public class VagaController {
@@ -30,5 +35,25 @@ public class VagaController {
         vr.save(vaga);
         attributes.addFlashAttribute("mensagem", "Vaga cadastrada com sucesso!");
         return "redirect:/cadastrarVaga";
+    }
+
+    @RequestMapping(value = "/vagas", method = RequestMethod.GET)
+    public ModelAndView listaVagas(){
+        ModelAndView mv = new ModelAndView("vaga/listaVaga");
+        Iterable<Vaga> vagas = vr.findAll();
+        mv.addObject("vagas", vagas);
+        return mv;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ModelAndView detalhesVaga(@PathVariable("id") long id){
+        Vaga vaga = vr.findById(id);
+        ModelAndView mv = new ModelAndView("vaga/detalhesVaga");
+        mv.addObject("vaga", vaga);
+
+        Iterable<Candidato> candidatos = cr.findByVaga(vaga);
+        mv.addObject("candidatos", candidatos);
+
+        return mv;
     }
 }
--- src/main/java/com/AppRH/AppRH/repository/CandidatoRepository.java
+++ src/main/java/com/AppRH/AppRH/repository/CandidatoRepository.java
@@ -1,4 +1,18 @@
 package com.AppRH.AppRH.repository;
 
-public interface CandidatoRepository {
+import com.AppRH.AppRH.models.Candidato;
+import com.AppRH.AppRH.models.Vaga;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CandidatoRepository extends CrudRepository<Candidato, String> {
+    Iterable<Candidato> findByVaga(Vaga vaga);
+
+    Candidato findByRg(String rg);
+
+    Candidato findById(long id);
+
+    List<Candidato> findByNameCandidato(String name);
+
 }
